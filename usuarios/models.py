33f62c5a826127d8d64ee58111d5e85807df1f5c
Sara Sánchez Restrepo <sarasanchezrestrepo@MacBook-Air-de-Sara-2.local>
2026-04-14 07:14:12 -0500

    agrega identificacion de rol en login

--- usuarios/models.py
+++ usuarios/models.py
@@ -78,3 +78,42 @@ class PlanCuidado(models.Model):
 
     def __str__(self):
         return f"Plan de {self.id_paciente.nombre}"
+
+class ActividadCuidado(models.Model):
+
+    id_actividad = models.AutoField(primary_key=True)
+    id_plan = models.ForeignKey(
+        PlanCuidado,
+        on_delete=models.CASCADE,
+        db_column='id_plan'
+    )
+    nombre_actividad = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50)
+    hora_programada = models.TimeField()
+    frecuencia = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'actividades_cuidado'
+
+    def __str__(self):
+        return self.nombre_actividad
+
+class RegistroDiario(models.Model):
+
+    id_registro = models.AutoField(primary_key=True)
+    id_actividad = models.ForeignKey(
+        ActividadCuidado,
+        on_delete=models.CASCADE,
+        db_column='id_actividad'
+    )
+    fecha = models.DateField()
+    realizada = models.BooleanField(null=True, blank=True)  # None = pendiente, True = realizada, False = no realizada
+    observacion = models.TextField(null=True, blank=True)
+    hora_registro = models.TimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'registro_diario'
+        unique_together = ('id_actividad', 'fecha')  # evita duplicados por día
+
+    def __str__(self):
+        return f"{self.id_actividad.nombre_actividad} - {self.fecha}"
