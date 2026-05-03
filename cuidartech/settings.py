b5cff89583284d6c5ea1dd14c9447ea332808003
Mateo-arrieta <luis.arrietaji@amigo.edu.co>
2026-05-03 15:32:58 -0500

    Update settings.py

--- cuidartech/settings.py
+++ cuidartech/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Después
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
